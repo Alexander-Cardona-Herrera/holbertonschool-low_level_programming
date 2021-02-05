f0f279d6ebc15b6b5a3329aef6f97afd59847d48
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-05 20:45:26 +0000

    adding 4-print_alphabt.c program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower = 'a';
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if ((lower == 'q') && (lower == 'e'))
+		{
+		putchar(lower);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
