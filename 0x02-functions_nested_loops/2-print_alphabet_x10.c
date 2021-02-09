f8f0969ebec45f3dae050bd910e043001d8ef56a
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-09 00:46:59 -0500

    adding files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - prints alphabet
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	
+}
