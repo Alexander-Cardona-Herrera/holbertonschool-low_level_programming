866d130073411cce68486989fe4dd35514414d8d
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-10 11:12:36 -0500

    some keys

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,13 @@ void jack_bauer(void)
 	for (hours = 0; hours < 24; hours++)
 	{
 		for (min = 0; min < 60; min++)
-
+		{
 		_putchar('0' + (hours / 10));
 		_putchar('0' + (hours % 10));
 		_putchar(':');
 		_putchar('0' + (min / 10));
 		_putchar('0' + (min % 10));
 		_putchar('\n');
+		}
 	}
 }
