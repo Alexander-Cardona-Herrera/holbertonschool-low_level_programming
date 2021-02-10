b616b7aac0ad8b43484bee931d6647b722e5f5e7
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-09 20:33:55 -0500

    adding 8-24_hours.c program

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - count the time of the day
+ *Return: 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int min;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (min = 0; min < 60; min++)
+
+		_putchar('0' + (hours / 10));
+		_putchar('0' + (hours % 10));
+		_putchar(':');
+		_putchar('0' + (min / 10));
+		_putchar('0' + (min % 10));
+		_putchar('\n');
+	}
+}
