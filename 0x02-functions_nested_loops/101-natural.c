a5d283069fb010e920ffdbae884ac237c131cf14
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-10 20:32:01 -0500

    some changes

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,16 +4,14 @@
  *main - entry point
  *Return: 0 (Success)
  */
-
 int main(void)
 {
 	int n, m = 0;
 
 	for (n = 0; n < 1024; n++)
-	{
 		if (n % 5 == 0 || n % 3 == 0)
 			m = m + n;
-	}
-	printf("%d ", m);
-	putchar('\n');
+
+	printf("%d\n", m);
+
 }
