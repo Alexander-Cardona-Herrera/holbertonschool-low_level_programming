571825b4258f2927bd74b124e3c64db8df5f0c3f
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-11 18:11:07 -0500

    dont compile

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,11 @@
 
 /**
  * _isupper - returns 1 if is uppercase, 0 otherwise.
- *
+ *@c: an ASCII character
  * Return: 1 or 0
  */
 int _isupper(int c)
 {
-	int c;
 
 	if (c >= 'A' && c <= 'Z')
 		return (1);
