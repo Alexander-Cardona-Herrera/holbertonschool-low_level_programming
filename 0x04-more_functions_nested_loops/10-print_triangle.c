fa87866ed935291f87b03a98057d5efda5781c19
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-11 20:40:12 -0500

    i know its wrong but...

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints triangles of n size
+ * @size: the size of the triangle
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+        _putchar(\n)
+    
+	for (a = 1; a <= n; a++)
+	{
+		for (b = 1; b < a; b++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
