9f07aaa6193a8e281a9584f3cac767bd6d2d30f0
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-11 19:31:11 -0500

    backwards

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,6 @@
 
 /**
  * print_line - prints n times '_'
- * @a: counter
  * @n: the times fot 'n? to print
  * Return: always 0
  */
@@ -10,10 +9,7 @@ void print_line(int n)
 {
 	int a;
 
-	if (n <= 0)
-		_putchar('\n');
-
-	for (a = 1; a <= n; a++)
+	for (a = n; a > 0; a--)
 	{
 		_putchar('_');
 	}
