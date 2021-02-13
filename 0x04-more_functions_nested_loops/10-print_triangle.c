c957bc95c0d4a5c69c7fe0d049c88b0b4af9a490
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-12 19:41:35 -0500

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,17 +7,24 @@
  */
 void print_triangle(int size)
 {
-	int a;
-	int b;
+	int fil, col, a, b, c;
+
+	fil = size;
+	col = size - 1;
 
 	if (size <= 0)
-        _putchar(\n)
-    
-	for (a = 1; a <= n; a++)
-	{
-		for (b = 1; b < a; b++)
-			_putchar(' ');
-		_putchar('\\');
 		_putchar('\n');
+
+	else
+	{
+		for (a = 1; a <= fil; a++)
+		{
+			for (b = 1; b <= col; b++)
+				_putchar(' ');
+			col--;
+			for (c = 1; c <= a ; c++)
+				_putchar('#');
+		_putchar('\n');
+		}
 	}
 }
