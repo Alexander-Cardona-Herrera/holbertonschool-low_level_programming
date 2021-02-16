db063439e4feb5771e856745b2f91545fd94d45e
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-16 14:05:36 -0500

    adding 0-reset_to_98.c holberton.h programs

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,16 @@
 #include "holberton.h"
 
 /**
+ * reset_to_98 - updates an integer
+ * @n: an integer
+ * return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int c = *n;
+
+	c = 98;
+	*n = c;
+
+}
