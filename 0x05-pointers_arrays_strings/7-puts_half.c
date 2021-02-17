58f4158871ea20677320b60b1bd81bfe5630d63e
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-16 19:23:15 -0500

    adding 7-puts_half.c function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print a string in reverse
+ * @str: pointer of the string
+ * Return: return a count
+ */
+
+void puts_half(char *str)
+{
+	int c;
+
+	for (c = 0 ; str[c] != '\0'; c++)
+	{
+	}
+
+	if (c % 2 == 0)
+	{
+		for (c /= 2; str[c] != '\0'; c++)
+		{
+			_putchar(str[c]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		c++;
+		for (c = 0 ; str[c] != '\0'; c++)
+		{
+			_putchar(str[c]);
+		}
+	_putchar('\n');
+	}
+}
