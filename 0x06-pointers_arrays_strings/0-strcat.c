06ebac11c912698e4e5573043ec704b690189368
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-18 18:08:45 -0500

    adding 0-strcat.c funtion

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include "_putchar.c"
+
+/**
+ * _strcat - concatenate strings
+ * @dest: destiny array
+ * @src: to concatenate array
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c, n;
+	char *start = dest;
+
+	for (c = 0 ; start[c] != '\0' ; c++)
+	{
+	}
+	for (n = 0 ; src[n] != '\0' ; n++)
+		start[c + n] = src[n];
+
+	start[c + n] = '\0';
+
+	return (start);
+}
