d9ea9eaea25723a45da9791e8731e27f9c470546
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-18 23:25:37 -0500

    adding 3-strcmp.c function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - comparate strings
+ * @s1: string
+ * @s2: string
+ * Return: comp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else if (*s1 > *s2)
+		return (15);
+	else
+		return (-15);
+}
