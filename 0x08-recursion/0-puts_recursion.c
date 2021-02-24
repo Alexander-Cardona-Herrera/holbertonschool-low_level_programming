940cafe353508dda8a6f065e0c1b6860a090d7d7
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-24 14:51:51 -0500

    adding spaces around ()

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
+		return (0);
 	}
-	else
-	{
 	_putchar(*s);
-	_puts_recursion(s+1);
-	}
+	_puts_recursion(s + 1);
 }
