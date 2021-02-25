7c393cafe8c714813da6bb4344d504d2235dd1d8
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-02-24 20:57:56 -0500

    adjusting betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,19 +11,25 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-	
+
 	if (n == 0)
 		return (0);
 
-	return (sqrt_compare (n , i));
+	return (sqrt_compare(n, i));
 }
+/**
+ * sqrt_compare - compare two integers
+ * @i: an integer
+ * @n: an integer
+ * Return: int
+ */
 int sqrt_compare(int n, int i)
 {
-	if( i * i == n)
+	if (i * i == n)
 		return (i);
-	
+
 	if (i * i > n)
 		return (-1);
-	
+
 	return (sqrt_compare(n, i + 1));
 }
