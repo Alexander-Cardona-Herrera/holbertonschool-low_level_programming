1de48180e8d562244d124a8e1b4f516949f67141
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-03-02 16:20:17 -0500

    adding 1-args.c program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * @argc: number of strings
+ * @argv: strings
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+	if (argv)
+
+	printf("%i\n", argc - 1);
+	return (0);
+}
