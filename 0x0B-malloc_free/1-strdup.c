f88fa8aff35416a96dc1112564d0aa47e002237c
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-03-03 14:18:46 -0500

    adding 1-strdup.c program

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/**
+ * _strdup - copy and malloc
+ * @str: an array
+ * Return: 0.
+ */
+char *_strdup(char *str)
+{
+	char *aux;
+	int i;
+	int j;
+
+	if (str == 0)
+		return (NULL);
+
+	for (i = 0; str[i] != 0; i++)
+		i++;
+
+	aux = malloc(i * sizeof(char));
+
+	if (!aux)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		aux[j]=str[j];
+
+	return (aux);
+}
