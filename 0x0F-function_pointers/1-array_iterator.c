31835e26bc148f80ae0397d4c6d366c080bbef5f
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-03-10 17:21:41 -0500

    adding 1-array_iterator.c function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints array
+ * @array: name of the person
+ * @size: size of the array
+ * @action: funtion
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL && action != NULL)
+
+	for (i = 0; i <= size - 1; i++)
+		action(array[i]);
+
+}
