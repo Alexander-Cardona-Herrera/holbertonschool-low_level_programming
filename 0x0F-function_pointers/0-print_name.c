cdf3ae99b790b19609cb6d860efc4629db73ccb1
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-03-10 16:07:33 -0500

    missing betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
 /**
  * print_name - prints a name
  * @name: name of the person
+ * @f: funtion
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 
-	return;
 }
