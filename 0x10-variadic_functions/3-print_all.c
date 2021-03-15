078cc983d2fea9954cd8b134db1040c4d79d14eb
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2021-03-15 12:20:16 -0500

    adding 3-print_all.c program

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print all the strings n
+ * @format: type of caracter matching
+ * Return: nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	char *p = "";
+	char *q = ", ";
+
+	formatos imp[] = {
+		{"c", caracter},
+		{"i", entero},
+		{"f", flotante},
+		{"s", linea},
+		{NULL, NULL},
+	};
+
+	va_list aux;
+
+	va_start(aux, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		for (j = 0; imp[j].s != NULL; j++)
+		{
+			if (format[i] == imp[j].s[0])
+			{
+				printf("%s", p);
+				imp[j].f(aux);
+				p = q;
+			}
+		}
+	}
+	va_end(aux);
+	printf("\n");
+}
+/**
+ * caracter - print all the characters
+ * @aux: va_list parameter
+ * Return: nothing.
+ */
+void caracter(va_list aux)
+{
+	int z;
+
+	z = va_arg(aux, int);
+	printf("%c", z);
+}
+/**
+ * entero - print all the integers
+ * @aux: va_list parameter
+ * Return: nothing.
+ */
+void entero(va_list aux)
+{
+	int z;
+
+	z = va_arg(aux, int);
+	printf("%d", z);
+}
+/**
+ * flotante - print all the floats
+ * @aux: va_list parameter
+ * Return: nothing.
+ */
+void flotante(va_list aux)
+{
+	double z;
+
+	z = va_arg(aux, double);
+	printf("%f", z);
+}
+/**
+ * linea - print all the strings
+ * @aux: va_list parameter
+ * Return: nothing.
+ */
+void linea(va_list aux)
+{
+	char *z;
+
+	z = va_arg(aux, char*);
+
+	if (z == NULL)
+		z = "(nil)";
+
+	printf("%s", z);
+}
